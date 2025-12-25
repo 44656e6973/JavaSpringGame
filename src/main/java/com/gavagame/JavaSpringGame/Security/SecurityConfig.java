1119a3d4d2300c84450ecaf9f90aedac717003ed
Ivanov Denis <den4ik2006Nike2016@yandex.ru>
2025-12-26 04:24:31 +0700

    0.3.3

--- src/main/java/com/gavagame/JavaSpringGame/Security/SecurityConfig.java
+++ src/main/java/com/gavagame/JavaSpringGame/Security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(
-                                "/",
+                                "/login",
                                 "/favicon.ico",
                                 "/css/**",
                                 "/js/**",
