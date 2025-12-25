5413e8004e65886c12c39160beca0087cb05ae90
Ivanov Denis <den4ik2006Nike2016@yandex.ru>
2025-12-26 03:05:49 +0700

    0.3.2

--- src/main/java/com/gavagame/JavaSpringGame/Security/SecurityConfig.java
+++ src/main/java/com/gavagame/JavaSpringGame/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -30,8 +31,7 @@ public class SecurityConfig {
                                 "/js/**",
                                 "/images/**",
                                 "/icons/**",
-                                "/static/**",
-                                "/check-file/**"
+                                "/static/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
@@ -56,8 +56,7 @@ public class SecurityConfig {
                         .maximumSessions(1)
                         .maxSessionsPreventsLogin(false)
                 );
-
-//                .csrf(AbstractHttpConfigurer::disable);
+                
 
         return http.build();
     }
--- src/main/java/com/gavagame/JavaSpringGame/Service/SecurityService.java
+++ src/main/java/com/gavagame/JavaSpringGame/Service/SecurityService.java
@@ -1,4 +1,25 @@
 package com.gavagame.JavaSpringGame.Service;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+@Service
 public class SecurityService {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    @PostConstruct
+    public void init() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+
+        privateKey = keyPairGenerator.generateKeyPair().getPrivate();
+        publicKey = keyPairGenerator.generateKeyPair().getPublic();
+    }
+
 }
